29cb5ae12849594283a6bd21dd3b31babf89d45a
Mauricio Leal <mauriciolealmx@gmail.com>
2019-03-27 18:14:51 -0500

    Added redis Url and own mongodb url.

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,8 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://readonly:password@ds063124.mlab.com:63124/blog_everyone',
-  cookieKey: '123123123'
+  mongoURI:
+    'mongodb+srv://emdbAdmin:32j1xTWIjgIg1q0a@email-marketing-kcs7f.mongodb.net/blog_dev?retryWrites=true',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
