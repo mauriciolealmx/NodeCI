bfab8d35308352b358c9d2eeb25231a1111f1d3a
Mauricio Leal <mauriciolealmx@gmail.com>
2019-03-29 09:50:01 -0500

    Remove dev.js from git's index.

--- config/dev.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  googleClientID:
-    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
-  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI:
-    'mongodb+srv://emdbAdmin:32j1xTWIjgIg1q0a@email-marketing-kcs7f.mongodb.net/blog_dev?retryWrites=true',
-  cookieKey: '123123123',
-  redisUrl: 'redis://127.0.0.1:6379'
-};
